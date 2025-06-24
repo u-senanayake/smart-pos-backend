d7521bf60df0e86b34e187d26b7eaf0f29812df4
Udayanga Senanayake <mailtoudayanga@gmail.com>
2025-06-24 18:16:49 +0900

    User role name changes

--- smartpos-user-service/src/main/resources/data.sql
+++ smartpos-user-service/src/main/resources/data.sql
@@ -1,17 +1,19 @@
 --Run
 --Role table
 INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
-VALUES ('Admin', 'Administrator role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
+VALUES ('admin', 'Administrator role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
 INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
-VALUES ('User', 'User role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
+VALUES ('sales', 'Sale role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
 INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
-VALUES ('Sales Associate', 'Sales Associate role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
+VALUES ('delivery', 'Delivery role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
 INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
-VALUES ('Inventory Manager', 'Inventory Manager role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
+VALUES ('purchase', 'Purchase role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
 INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
-VALUES ('Customer Service', 'Customer Service role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
+VALUES ('accountant', 'Accountant role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
 INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
-VALUES ('Deleted', 'Deleted role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, CURRENT_TIMESTAMP);
+VALUES ('product_manager', 'Accountant role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, NULL);
+INSERT INTO role (role_name, description, enabled, created_at, updated_at, deleted, deleted_at)
+VALUES ('deleted', 'Deleted role', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true, CURRENT_TIMESTAMP);
 
 --User table
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
@@ -23,31 +25,37 @@ VALUES ('admin_user', 'Admin', 'User', 'admin.user@example.com', '123 Main St, C
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
                    role_id, password, enabled, locked, created_at, updated_at,
                    created_user_id, updated_user_id, deleted, deleted_at, deleted_user_id)
-VALUES ('john_doe', 'John', 'Doe', 'john.doe@example.com', '456 Elm St, City, Country', '1112223333', '3332221111', 2,
-        '$2a$10$abcdefghijklmnopqrstuv', TRUE, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1, FALSE, NULL, NULL);
+VALUES ('sale_user', 'Sale', 'User', 'john.doe@example.com', '456 Elm St, City, Country', '1112223333', '3332221111', 2,
+        '$2a$10$xu85axo.lRg.SgTmegtZj.ica94BKMnkvPmlFsmauxYZJ3fMsACCu', TRUE, FALSE, CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP, 1, 1, FALSE, NULL, NULL);
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
                    role_id, password, enabled, locked, created_at, updated_at,
                    created_user_id, updated_user_id, deleted, deleted_at, deleted_user_id)
-VALUES ('jane_smith', 'Jane', 'Smith', 'jane.smith@example.com', '789 Oak St, City, Country', '4445556666',
-        '6665554444', 3, '$2a$10$abcdefghijklmnopqrstuv', TRUE, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1,
+VALUES ('delivery_user', 'Delivery', 'User', 'jane.smith@example.com', '789 Oak St, City, Country', '4445556666',
+        '6665554444', 3, '$2a$10$xu85axo.lRg.SgTmegtZj.ica94BKMnkvPmlFsmauxYZJ3fMsACCu', TRUE, FALSE, CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP, 1, 1,
         FALSE, NULL, NULL);
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
                    role_id, password, enabled, locked, created_at, updated_at,
                    created_user_id, updated_user_id, deleted, deleted_at, deleted_user_id)
-VALUES ('peter_parker', 'Peter', 'Parker', 'peter.parker@example.com', '101 Maple St, City, Country', '7778889999',
-        '9998887777', 2, '$2a$10$abcdefghijklmnopqrstuv', TRUE, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1,
+VALUES ('purchase_user', 'Purchase', 'User', 'peter.parker@example.com', '101 Maple St, City, Country', '7778889999',
+        '9998887777', 4, '$2a$10$xu85axo.lRg.SgTmegtZj.ica94BKMnkvPmlFsmauxYZJ3fMsACCu', TRUE, FALSE, CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP, 1, 1,
         FALSE, NULL, NULL);
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
                    role_id, password, enabled, locked, created_at, updated_at,
                    created_user_id, updated_user_id, deleted, deleted_at, deleted_user_id)
-VALUES ('bruce_wayne', 'Bruce', 'Wayne', 'bruce.wayne@example.com', '500 Gotham Ave, Gotham City', '2223334444',
-        '4443332222', 1, '$2a$10$abcdefghijklmnopqrstuv', TRUE, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1,
+VALUES ('accountant_user', 'Accountant', 'User', 'bruce.wayne@example.com', '500 Gotham Ave, Gotham City', '2223334444',
+        '4443332222', 5, '$2a$10$xu85axo.lRg.SgTmegtZj.ica94BKMnkvPmlFsmauxYZJ3fMsACCu', TRUE, FALSE, CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP, 1, 1,
         FALSE, NULL, NULL);
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
                    role_id, password, enabled, locked, created_at, updated_at,
                    created_user_id, updated_user_id, deleted, deleted_at, deleted_user_id)
-VALUES ('clark_kent', 'Clark', 'Kent', 'clark.kent@example.com', '600 Metropolis St, Metropolis', '5556667777',
-        '7776665555', 3, '$2a$10$abcdefghijklmnopqrstuv', TRUE, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 1,
+VALUES ('product_manager', 'Product', 'Manager', 'clark.kent@example.com', '600 Metropolis St, Metropolis',
+        '5556667777',
+        '7776665555', 6, '$2a$10$xu85axo.lRg.SgTmegtZj.ica94BKMnkvPmlFsmauxYZJ3fMsACCu', TRUE, FALSE, CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP, 1, 1,
         FALSE, NULL, NULL);
 INSERT INTO Users (username, first_name, last_name, email, address, phone_no_1, phone_no_2,
                    role_id, password, enabled, locked, created_at, updated_at,
